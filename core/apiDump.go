29df2136a24561e6ed31f13148e87f1b1aa16467
MLuidjy <luidjymongorinsemedobrito@ynov.com>
2024-02-02 11:43:48 +0100

    struct

--- core/apiDump.go
+++ core/apiDump.go
@@ -1,1 +1,34 @@
 package core
+
+type artist struct {
+	id           int
+	image        string
+	nom          string
+	member       []string
+	creaDate     string
+	firstalbum   string
+	locations    string
+	concertdates string
+	relations    string
+}
+
+type locations struct {
+	index     []string // se compose des trois variables ci-dessous
+	id        int
+	locations []string
+	dates     string
+}
+
+type dates struct {
+	index []string
+	id    int
+	dates []string
+}
+
+type relations struct {
+	index         []string
+	id            int
+	ville         string
+	dates         string
+	dateslocation [][]string
+}
